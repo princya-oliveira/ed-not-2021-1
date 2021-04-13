c17060e7c1fe57dc6f1cbe607e0eb13fcfb64053
princya-oliveira <princya.oliveira@outlook.com>
2021-04-13 01:01:32 +0000

    (12/04) correção atividade de revisão

--- ex-revisao01.mjs
+++ ex-revisao01.mjs
@@ -7,35 +7,45 @@
        por SG_UE e depois por NR_CANDIDATO.
 */
 
-let comps
+/*
+    MAPEAMENTO DAS VARIÁVEIS
+    a -> função de ordenação
+    b -> vetor a ser ordenado
+    c -> subfunção que encontra o menor valor no restante do vetor
+    d -> parâmetro da subfunção (vetor)
+    e -> parâmetro da subfunção (posição inicial do subvetor)
+    f -> valor inicial igual a e, posição do menor valor no subvetor
+    g -> contador do for da subfunção, começa no e + 1 e vai até o penúltimo valor de d
+    h -> contador do for da função principal inicia em 0 e vai até a penúltima posição
+    i -> o resultado da chamada à subfunção c (menor do resto do vetor)
+*/
 
-//Selection Sort
+// SELECTION SORT
 const a = (b, fnComp) => {
-
-    comps = 0
 
     const c = (d, e) => {
         let f = e //let menor = inicio
         //Loop até a última posição
-        for(let g = e + 1; g < d.length; g++) if(fnComp(d[g] < d[f])) f = g 
-        comps++
+        //for(let g = e + 1; g < d.length; g++) if(fnComp(d[g] < d[f])) f = g
+        for(let g = e + 1; g < d.length; g++) if(fnComp(d[f], d[g])) f = g //quando o sinal for de < tem que inverter o sinal
         return f //menor valor
     }
     //Percurso do vetor até a penúltima posição
     for(let h = 0; h < b.length - 1; h++) {
         let i = c(b, h + 1)
-        if(fnComp(b[h] > b[i])) [b[h], b[i]] = [b[i], b[h]]
-        comps++
+        //if(fnComp(b[h] > b[i])) [b[h], b[i]] = [b[i], b[h]]
+        if(fnComp(b[h], b[i])) [b[h], b[i]] = [b[i], b[h]] //quando o sinal for > é só trocá-lo pela virgula
     }
 }
 
 import { candidatos } from './includes/candidatos-2018.mjs'
 
-//console.log('ANTES:', candidatos)
-console.time('Ordenando candidatos...')
-// Ordenando pelo nome de urna (SG_UE)
-a(candidatos, (obj1, obj2) => obj1.SG_UE > obj2.SG_UE)
-console.timeEnd('Ordenando candidatos...')
-console.log('DEPOIS:', candidatos)
-console.log({comps})
+a(candidatos, (a, b) => {
+    if(a.SG_UE === b.SG_UE) {
+        return a.NR_CANDIDATO > b.NR_CANDIDATO
+    }
+    else return a.SG_UE > b.SG_UE
+})
+
+console.log(candidatos)
 
