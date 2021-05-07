d26ef43330e63526eb66ff53aeefdf5722422101
princya-oliveira <princya.oliveira@outlook.com>
2021-05-07 01:23:42 +0000

    (06/05) estrutura deque

---
+++ 19-deque1.mjs
@@ -0,0 +1,36 @@
+import { Deque } from './lib/deque.mjs'
+
+let deque = new Deque()
+
+deque.insertBack('Margarida')
+deque.insertBack('Pateta')
+deque.insertBack('Gastão')
+deque.insertBack('Donald')
+
+console.log(deque.print())
+
+deque.insertFront('Tio Patinhas')
+console.log(deque.print())
+
+deque.insertFront('Minie')
+console.log(deque.print())
+
+let primeiro = deque.peekFront()
+let ultimo = deque.peekBack()
+console.log({primeiro, ultimo})
+console.log(deque.print())
+
+let atendido = deque.removeFront()
+console.log({atendido})
+console.log(deque.print())
+
+let desistente = deque.removeBack()
+console.log({desistente})
+console.log(deque.print())
+
+deque.insertBack('Mickey')
+console.log(deque.print())
+
+
+
+
